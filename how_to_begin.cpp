276d5ea9eb18147003809b58375d53c747fe4886
Way-Onehh <2726077791@qq.com>
2024-08-12 15:18:46 +0800

    init this repo

---
+++ how_to_begin.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<thread>
+int main(int n,char* argv[])
+{
+    //Avoid polluting namespaces
+    using std::cout;
+    using std::endl;
+    using std::thread;
+    using std::this_thread::get_id;
+
+    //Function entry to print something and thread  id; 
+    auto &&la= [](int times)
+    {
+        for (size_t i = 0; i < times; i++)
+        {
+        cout<<"wr is handsome man"<<" : "<<std::this_thread::get_id()<<endl;
+        }
+    };
+    
+    //Create thread by Function entry , pass the parameters, and wait the thread executing.
+    auto &&th = thread(la,100);
+    la(10);
+    th.join();
+
+
+    return 0;
+}
